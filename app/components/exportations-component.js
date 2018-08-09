dc42312b5d379666c29ccc3d6cac025410d73fbd
Nicole Escobar <wawandco@Nicoles-MacBook-Pro.local>
2018-08-08 19:52:11 -0500

    fixing responsive

--- app/components/exportations-component.js
+++ app/components/exportations-component.js
@@ -12,22 +12,26 @@ export default Component.extend({
       var clouds2 = document.getElementById("clouds2");
       var sectionAbout = document.getElementById("AboutUs");
 
-      if(window.visualViewport.pageTop >= 2250){
-        waves.classList.add("visible");
-        cloud.classList.add("visible");
-        clouds.classList.add("visible");
-        clouds2.classList.add("visible");
-      } else {
-        waves.classList.remove("visible");
-        cloud.classList.remove("visible");
-        clouds.classList.remove("visible");
-        clouds2.classList.remove("visible");
-      }
+      if(window.innerWidth > 600) {
+        
+        if(window.visualViewport.pageTop >= 2250){
+          waves.classList.add("visible");
+          cloud.classList.add("visible");
+          clouds.classList.add("visible");
+          clouds2.classList.add("visible");
+        } else {
+          waves.classList.remove("visible");
+          cloud.classList.remove("visible");
+          clouds.classList.remove("visible");
+          clouds2.classList.remove("visible");
+        }
 
-      if(window.visualViewport.pageTop >= 30){
-        sectionAbout.classList.add("visible");
-      } else {
-        sectionAbout.classList.remove("visible");
+        if(window.visualViewport.pageTop >= 30){
+          sectionAbout.classList.add("visible");
+        } else {
+          sectionAbout.classList.remove("visible");
+        }
+
       }
     };
   },
